cf2266062af861e3cd2a0db417ceaae1a1e09e60
ermyas7 <ermyas.fekadu@gmail.com>
2019-03-17 19:15:00 +0530

    Add TODO APP UI

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+import Main from './src/components/Main';
+class App extends Component{
+  render() {
+    return (
+      <Main/>
+    );
+  }
+}
+
+export default App;
